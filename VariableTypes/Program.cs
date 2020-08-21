b6fde6267444c1c22a08174fb2f27bc4fded810a
wbrunovieira <wbrunovieira77@gmail.com>
2020-08-21 17:48:43 +0100

    First Commit

---
+++ VariableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariableTypes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string nameDog = "Teddy";
+            int dogAge = 1;
+            char firstLetter = 'T';
+            bool likePlay = true;
+            double toys = 12;
+            decimal weigth = 70m;
+
+                Console.WriteLine($"My dog name's is {nameDog}, he is {dogAge} old, the first letter'name is {firstLetter}. If you ask me he loves to play it is so {likePlay}. He has {toys} to play all day long and keep his {weigth} pounds");
+        }
+    }
+}
